430ad81da91e697cc724b81f82575eaf4b0c29be
Ivan Havrylyshyn <jyjuk1@gmail.com>
2024-10-02 13:04:33 +0300

    Solution2

--- car/serializers.py
+++ car/serializers.py
@@ -4,9 +4,10 @@ from car.models import Car
 
 
 class CarSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
     manufacturer = serializers.CharField(max_length=64)
     model = serializers.CharField(max_length=64)
-    horse_powers = serializers.IntegerField(min_value=0, max_value=1000)
+    horse_powers = serializers.IntegerField(min_value=1, max_value=1914)
     is_broken = serializers.BooleanField()
     problem_description = serializers.CharField(
         required=False,
--- main.py
+++ main.py
@@ -1,4 +1,8 @@
+import io
 import json
+
+from rest_framework.parsers import JSONParser
+from rest_framework.renderers import JSONRenderer
 
 from car.models import Car
 from car.serializers import CarSerializer
@@ -6,12 +10,11 @@ from car.serializers import CarSerializer
 
 def serialize_car_object(car: Car) -> bytes:
     serializer = CarSerializer(car)
-    return json.dump(serializer.data)
+    return JSONRenderer().render(serializer.data)
 
 
-def deserialize_car_object(json_data: bytes) -> Car:
-    data = json_data.loads(json_data)
-    serializer = CarSerializer(data=data)
-    serializer.is_valid(raise_exception=True)
-    serializer.save()
-    return serializer
+def deserialize_car_object(json: bytes) -> Car:
+    stream = io.BytesIO(json)
+    data = JSONParser().parse(stream)
+    return Car(**data)
+
